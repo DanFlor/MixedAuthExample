3e6833e3d308b1b9132b676cd5fcd1dcd57d3979
Daniel Flor <dan.flor@objectpartners.com>
2018-05-02 10:35:34 -0500

    Injection trajectory complete.

--- app/src/main/java/com/obidan/mixedauthexample/injection/NetModule.kt
+++ app/src/main/java/com/obidan/mixedauthexample/injection/NetModule.kt
@@ -1,7 +1,90 @@
 package com.obidan.mixedauthexample.injection
 
+import android.content.Context
+import com.franmontiel.persistentcookiejar.PersistentCookieJar
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.obidan.mixedauthexample.api.MixedAuthAPI
+import com.obidan.mixedauthexample.api.OauthRefreshAuthenticator
+import com.obidan.mixedauthexample.util.SecuredCredentialStorage
 import dagger.Module
+import dagger.Provides
+import okhttp3.CookieJar
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 class NetModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        val logger = HttpLoggingInterceptor()
+        logger.level = HttpLoggingInterceptor.Level.BODY
+        return logger
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        val gsonBuilder = GsonBuilder()
+        gsonBuilder.setLenient()
+        gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
+        return gsonBuilder.create()
+    }
+
+    @Provides
+    fun provideGsonConvertorFactory(gson: Gson): GsonConverterFactory {
+        return GsonConverterFactory.create(gson)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCookieJar(context: Context): CookieJar {
+        return PersistentCookieJar(
+                SetCookieCache(),
+                SharedPrefsCookiePersistor(context)
+        );
+    }
+
+    @Provides
+    @Singleton
+    fun provideOauthRefreshAuthenticator(
+            securedCredentialStorage: SecuredCredentialStorage
+    ): OauthRefreshAuthenticator {
+        return OauthRefreshAuthenticator(
+            securedCredentialStorage
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideMixedAuthAPI(
+            securedCredentialStorage: SecuredCredentialStorage,
+            gsonConverterFactory: GsonConverterFactory,
+            httpLoggingInterceptor: HttpLoggingInterceptor,
+            oauthRefreshAuthenticator: OauthRefreshAuthenticator,
+            cookieJar: CookieJar
+    ): MixedAuthAPI {
+
+        // Instantiate the concrete API:
+        val api = MixedAuthAPI.create(
+                securedCredentialStorage,
+                gsonConverterFactory,
+                httpLoggingInterceptor,
+                oauthRefreshAuthenticator,
+                cookieJar
+        )
+
+        // Hand the API back to the storage, so it can refresh!
+        securedCredentialStorage.api = api
+
+        //Done!
+        return api
+    }
+
 }
